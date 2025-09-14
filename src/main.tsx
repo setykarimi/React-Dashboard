2492464049bbeba87a90e377c4abe191e00b4bdb
ISQI\se.karimi <setykarimi80@gmail.com>
2025-09-14 12:31:22 +0330

    🌿feat: Createroutes in main page

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,21 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+import App from './App';
 
-createRoot(document.getElementById('root')!).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+]);
+
+
+const root = document.getElementById("root");
+createRoot(root!).render(
   <StrictMode>
-    <App />
+     <RouterProvider router={router} />,
   </StrictMode>,
 )
