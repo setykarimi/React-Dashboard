ea8545d355508ce12406b76c870b0435b06e8685
ISQI\se.karimi <setykarimi80@gmail.com>
2025-09-21 16:22:46 +0330

    🌿feat: Add sidebar static

---
+++ public/statics/menu.ts
@@ -0,0 +1,10 @@
+export const sidebar = [
+    { title: "Dashboard", icon : "" },
+    { title: "Analytics", icon : "" },
+    { title: "Invoice", icon : "" },
+    { title: "Schedule", icon : "" },
+    { title: "Calendar", icon : "" },
+    { title: "Messages", icon : "" },
+    { title: "Notification", icon : "" },
+    { title: "Settings", icon : "" }
+]
