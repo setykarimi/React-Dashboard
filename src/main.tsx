b59bc9f6071207bcbfd0066be29a1334d0dd6307
ISQI\se.karimi <setykarimi80@gmail.com>
2025-09-21 11:06:04 +0330

    🌿feat: Change app router for dashboard layout

--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 import { createBrowserRouter } from "react-router";
 import { RouterProvider } from "react-router/dom";
-import App from './App';
+import App from "./App";
+import Layout from "./components/layout";
+import "./index.css";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App />,
+    Component: Layout,
+    children: [{ index: true, Component: App }],
   },
 ]);
-
 
 const root = document.getElementById("root");
 createRoot(root!).render(
   <StrictMode>
-     <RouterProvider router={router} />,
-  </StrictMode>,
-)
+    <RouterProvider router={router} />,
+  </StrictMode>
+);
